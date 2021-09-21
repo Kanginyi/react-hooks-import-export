0e5096ba229a05b237f4d0c370acdc76cd1e9239
Eric Yi <erickanginyi@gmail.com>
2021-09-20 23:14:14 -0400

    commit tree 7e2245becb4a914aec96336a01965a3354de2ed0

--- src/Hogwarts.js
+++ src/Hogwarts.js
@@ -1,8 +1,22 @@
 import React from "react";
 import whoseHouse from "./houses/whoseHouse";
+import Hufflepuff from "./houses/Hufflepuff";
+import {colors, gryffMascot} from "./houses/Gryffindor";
 
 function Hogwarts() {
   whoseHouse(); // => "HAGRID'S HOUSE!"
 
-  return <h1>Welcome to Hogwarts!</h1>;
+  console.log(colors);
+  console.log(gryffMascot());
+
+  return (
+    <>
+      <h1>Welcome to Hogwarts!</h1>
+      <div>
+        <Hufflepuff />
+      </div>
+    </>
+  )
 }
+
+export default Hogwarts;
